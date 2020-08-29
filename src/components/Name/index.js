b0cd21ce207fa7c8a1ec450973b81186e5340dca
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 18:21:12 -0500

    created name and paint component

---
+++ src/components/Name/index.js
@@ -0,0 +1,12 @@
+//import react
+import React from 'react'
+
+//create functional component and export it
+export default function Name() {
+  
+  return (
+    <label className="header-name">
+      
+    </label>
+  )
+}
---
+++ src/components/Paint/index.js
@@ -0,0 +1,13 @@
+//import react
+import React from 'react'
+//import Name component
+import Name from '../Name'
+
+//created functional component and export it
+export default function Paint() {
+  return (
+    <div className="app">
+      <Name />
+    </div>
+  )
+}

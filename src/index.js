cf4937951121f3a9af2be2851746363ca3e9c63b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 18:06:03 -0500

    add react router

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//import react
+import React from 'react';
+//import render method
+import { render } from 'react-dom';
+//import hash router
+import { HashRouter as Router } from 'react-router-dom';
+
+//root const
+const root = document.getElementById('root');
+
+//render the app
+render(
+	<Router basename='/'>
+		<App />
+	</Router>,
+	root,
+);

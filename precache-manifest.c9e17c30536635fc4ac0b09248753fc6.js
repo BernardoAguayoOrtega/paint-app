9b0ca5004cd37d9f5050c26238d1edc54972ed83
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-29 18:11:36 -0500

    Updates

---
+++ precache-manifest.c9e17c30536635fc4ac0b09248753fc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db47318a3648e80c89da523afe1a4f9a",
+    "url": "/paint-app/index.html"
+  },
+  {
+    "revision": "684d812d736fcf3f0c0a",
+    "url": "/paint-app/static/js/2.9bd4801f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/paint-app/static/js/2.9bd4801f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d033619e3a0936addeb4",
+    "url": "/paint-app/static/js/main.836bcc9a.chunk.js"
+  },
+  {
+    "revision": "ac8120db0c2689064d8f",
+    "url": "/paint-app/static/js/runtime-main.58c6a7e6.js"
+  }
+]);
